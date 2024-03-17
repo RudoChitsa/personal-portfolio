bbb82b3e64cbc93d30d596fc030489c3fd3dc2b2
RudoChitsa <elichitsa@gmail.com>
2024-03-17 17:15:17 +0200

    created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import './index.css';
-import AboutMe from './components/AboutMe';
-import Education from './components/Education';
-import Navigation from './components/Navigation';
-import Bio from './components/Bio';
+import Navbar from "./components/NavBar/navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navigation />
-        <Bio />
-        <AboutMe />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Education />
+      <Navbar />
     </div>
   );
 }
