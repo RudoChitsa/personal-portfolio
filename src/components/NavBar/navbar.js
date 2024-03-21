9e5439ca77f6e1572577cea8f05545b5b7f0a25a
RudoChitsa <elichitsa@gmail.com>
2024-03-21 13:17:03 +0200

    mobile menu and logo

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './navbar.css';
-import logo from '../../assets/close.png';
+import logo from '../../assets/logo.png';
 import contactImg from '../../assets/contact.png';
 import { Link } from 'react-scroll';
 import Menu from '../../assets/menu.png';
@@ -14,7 +14,6 @@ const Navbar = () => {
       <div className='desktopMenu'>
         <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
         <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>About</Link>
-        <Link activeClass='active' to='education' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Education</Link>
         <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Portfolio</Link>
         <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-80} duration={500} className='desktopMenuListItem'>Clients</Link>
       </div>
